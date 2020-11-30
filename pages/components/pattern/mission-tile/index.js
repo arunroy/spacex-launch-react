205c7b1aacac0e7ce7f842f8b99d1d9885aa3436
arun.roy <arun.iamwatiam@gmail.com>
2020-11-30 12:15:50 +0530

    added check for presence of value

--- pages/components/pattern/mission-tile/index.js
+++ pages/components/pattern/mission-tile/index.js
@@ -1,17 +1,22 @@
 import styles from './mission-tile.module.scss';
 
 export default function MissionTile({ id, data }) {
-    return <div className={ styles.missionTile} aria-label={`mission details for flight number ${ data.flight_number }`}>
-        <img src = { data.links.mission_patch_small} alt='space mission logo'></img>
+
+    MissionTile.defaultProps = {
+        id: null,
+        data: null
+    }
+    return <div className={ styles.missionTile} aria-label={`mission details for flight number ${ data && data.flight_number }`}>
+        <img src = { data && data.links && data.links.mission_patch_small} alt='space mission logo'></img>
         <span className={ styles.missionName } id='missionName'>
-            { `${ data.mission_name } #${ data.flight_number }`}
+            { `${ data && data.mission_name } #${ data && data.flight_number }`}
         </span>
         <span> 
-            { `Mission ids: ${ data.mission_id.map( missionId => (` ${ missionId }`))}` }
+            { `Mission ids: ${ data && data.mission_id.map( missionId => (` ${ missionId }`))}` }
         </span>
-        <span>{ `Launch Year : ${ data.launch_year }` }</span>
-        <span>{ `Successful Launch : ${ data.launch_success }` }</span>
-        <span>{ `Successful Landing : ${ data.launch_landing || '' }` }</span>
+        <span>{ `Launch Year : ${ data && data.launch_year }` }</span>
+        <span>{ `Successful Launch : ${ data && data.launch_success }` }</span>
+        <span>{ `Successful Landing : ${ data && data.launch_landing || '' }` }</span>
         
     </div>
 }
